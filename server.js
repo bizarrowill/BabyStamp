8da45b245ce1bbf1eec6e2a686b0f22e35cbb78a
William Robertson <williamp.robertson@gmail.com>
2018-02-22 18:36:10 -0700

    updating framework - moved controller to html-routes

--- server.js
+++ server.js
@@ -25,9 +25,18 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
