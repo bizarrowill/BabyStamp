951ffb473ed71d330d89c4426645bfed58a7f22a
David Thompson <drthusker@gmail.com>
2018-03-01 20:12:47 -0700

    heroku update 1.0

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ SALT_WORK_FACTOR = 12;
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -81,7 +81,7 @@ db.sequelize.sync()
 	.then(function(err){
 		});
 		app.listen(PORT, function() {
-			console.log("App listening on PORT: " + PORT);
+			console.log("App listening on PORT: " + port);
 	});
 module.exports = app
 //dave
